8f7ccc66cbfc06268eb51e7490e89543ddf4b511
leejaehong <ljh890322@gmail.com>
2024-02-02 16:01:55 +0900

    add command util
    Signed-off-by: leejaehong <ljh890322@gmail.com>

---
+++ agent/pkg/util/command.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func execCommand(command string, args ...string) (string, error) {
+	// Create an *exec.Cmd instance for the given command and its arguments
+	cmd := exec.Command(command, args...)
+
+	// A buffer to capture the standard output
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	// Run the command and wait for it to finish
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	// Return the captured standard output as a string
+	return stdout.String(), nil
+}
