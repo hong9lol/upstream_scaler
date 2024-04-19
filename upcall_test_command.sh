59ee6c22b528660c6cd452e940b559c2145ff5d4
Uncle Jake <ljh890322@gmail.com>
2024-04-20 00:48:38 +0900

    Create upcall_test_command.sh

---
+++ upcall_test_command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ====== TEST Start ======
+
+# limits=( 20 30 40 50 60 )
+limits=( 30 45 60 75 )
+for limit in ${limits[@]}
+    do    
+        python3 limits.py $limit
+        SET=$(seq 0 5)
+        for i in $SET
+            do  
+                echo $limit $i
+                #./init_test_env_kind_with_deathstarbench_socialnetwork.sh default
+                #./init_test_env_kind_with_deathstarbench_socialnetwork.sh fast
+                ./init_test_env_kind_with_deathstarbench_socialnetwork.sh 
+            done
+    done
+
+echo ====== TEST Done ======
