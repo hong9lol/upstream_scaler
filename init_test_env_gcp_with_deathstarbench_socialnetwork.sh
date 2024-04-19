d34333591aa7608f44984986e87386dcc6c4f81c
Uncle Jake <ljh890322@gmail.com>
2024-04-20 00:47:45 +0900

    Update init_test_env_gcp_with_deathstarbench_socialnetwork.sh

--- init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+++ init_test_env_gcp_with_deathstarbench_socialnetwork.sh
@@ -8,44 +8,44 @@ sudo sysctl -w fs.inotify.max_user_instances=2099999999
 sudo sysctl -w fs.inotify.max_queued_events=2099999999
 
 echo 1. Delete Previous Environment and Create new Environment
-if [ "$1" = "fast" ]; then
-    kind create cluster --name my-cluster --config ./yaml/kind/kind_1s.yaml
-else
-    kind create cluster --name my-cluster --config ./yaml/kind/kind.yaml
-fi
+#if [ "$1" = "fast" ]; then
+#    kind create cluster --name my-cluster --config ./yaml/kind/kind_1s.yaml
+#else
+#    kind create cluster --name my-cluster --config ./yaml/kind/kind.yaml
+#fi
 
 
 echo 2. Install application
-echo 2-1. Install MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app=metallb \
-                --timeout=90s
+#echo 2-1. Install MetalLB
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+#kubectl wait --namespace metallb-system \
+#                --for=condition=ready pod \
+#                --selector=app=metallb \
+#                --timeout=90s
 
 echo 2-2. IP address pool for service loadbalancing
-baseIP=$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '/[[:space:]]/ {print $1}' | grep -oE '[0-9]+\.[0-9]+\.[0-9]')
-export KIND_IP_RANGE="${baseIP}.200-${baseIP}.250"
-echo $KIND_IP_RANGE
-envsubst < yaml/metal-lb/metal-lb.yaml | kubectl apply -f-
-sleep 10
+#baseIP=$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '/[[:space:]]/ {print $1}' | grep -oE '[0-9]+\.[0-9]+\.[0-9]')
+#export KIND_IP_RANGE="${baseIP}.200-${baseIP}.250"
+#echo $KIND_IP_RANGE
+#envsubst < yaml/metal-lb/metal-lb.yaml | kubectl apply -f-
+#sleep 10
 
 echo 2-3. Install helm packages
-kind load docker-image mongo:4.4.6 -n my-cluster # move local image to kind cluster nodes
-kind load docker-image redis:6.2.4 -n my-cluster
-kind load docker-image memcached:1.6.7 -n my-cluster
-kind load docker-image alpine/git:2.43.0 -n my-cluster
-kind load docker-image yg397/openresty-thrift:xenial -n my-cluster
-kind load docker-image yg397/media-frontend:xenial -n my-cluster
-kind load docker-image jaegertracing/all-in-one:1 -n my-cluster
-kind load docker-image deathstarbench/social-network-microservices:0.3.0 -n my-cluster
+#kind load docker-image mongo:4.4.6 -n my-cluster # move local image to kind cluster nodes
+#kind load docker-image redis:6.2.4 -n my-cluster
+#kind load docker-image memcached:1.6.7 -n my-cluster
+#kind load docker-image alpine/git:2.43.0 -n my-cluster
+#kind load docker-image yg397/openresty-thrift:xenial -n my-cluster
+#kind load docker-image yg397/media-frontend:xenial -n my-cluster
+#kind load docker-image jaegertracing/all-in-one:1 -n my-cluster
+#kind load docker-image deathstarbench/social-network-microservices:0.3.0 -n my-cluster
 
 kubectl create secret docker-registry secret-jake --docker-username=hong9lol --docker-password=dlwoghd12@ 
 kubectl create secret docker-registry secret-jake --docker-username=hong9lol --docker-password=dlwoghd12@ -n upstream-system
 
 kubectl delete horizontalpodautoscalers.autoscaling --all=true --now=true --wait=true
 helm uninstall social-network --wait
-sleep 30
+sleep 330
 helm install social-network --wait ./DeathStarBench/socialNetwork/helm-chart/socialnetwork/
 
 echo 3. Start Metrics-server
