7fe69828c0419c836ed47bb3989ae2e7d4fd4d85
Uncle Jake <ljh890322@gmail.com>
2024-04-21 11:01:06 +0900

    Update upcall_test_command.sh

--- upcall_test_command.sh
+++ upcall_test_command.sh
@@ -7,14 +7,17 @@ limits=( 30 45 60 75 )
 for limit in ${limits[@]}
     do    
         python3 limits.py $limit
-        SET=$(seq 0 5)
-        for i in $SET
-            do  
-                echo $limit $i
-                ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
-                ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
-                ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
-            done
+        ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
+        ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
+        ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+        #SET=$(seq 0 5)
+        #for i in $SET
+        #    do  
+        #        echo $limit $i
+        #        ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
+        #       ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
+        #       ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+        #    done
     done
 
 echo ====== TEST Done ======
