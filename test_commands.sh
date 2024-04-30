5f42ddf8c4167951bf513a24af4639020736e1d4
leejaehong <ljh890322@gmail.com>
2024-04-30 04:09:52 +0000

    update

--- test_commands.sh
+++ test_commands.sh
@@ -3,7 +3,7 @@
 echo ====== TEST Start ======
 
 # limits=( 20 30 40 50 60 )
-limits=( 30 45 60 75 )
+limits=( 60 75 )
 for limit in ${limits[@]}
     do    
         python3 limits.py $limit
@@ -11,9 +11,9 @@ for limit in ${limits[@]}
         for i in $SET
             do  
                 echo $limit $i
-                ./init_test_env_kind_with_deathstarbench_socialnetwork.sh default
-                ./init_test_env_kind_with_deathstarbench_socialnetwork.sh fast
-                ./init_test_env_kind_with_deathstarbench_socialnetwork.sh 
+		./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
+		./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
+		./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
             done
     done
 
--- upcall_test_command.sh
+++ upcall_test_command.sh
@@ -3,19 +3,29 @@
 echo ====== TEST Start ======
 
 # limits=( 20 30 40 50 60 )
-limits=( 30 45 60 75 )
+limits=( 60 75 )
 #limits=( 100 125 150 200 )
 for limit in ${limits[@]}
     do    
         python3 limits.py $limit
-        SET=$(seq 0 5)
+        SET=$(seq 0 2)
         for i in $SET
-           do  
-               echo $limit $i
-               ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
-              ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
-              ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+            do  
+	        echo $limit $i
+	       	VAL1=60
+		if [ ${limit} != ${VAL1} ] ; then
+#			./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
+			./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
+			./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+#			echo 1
+		else
+			./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
+#			echo 2
+		fi
+#               ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh default
+#              ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh fast
+#              ./init_test_env_gcp_with_deathstarbench_socialnetwork.sh
            done
     done
 
-echo ====== TEST Done ======
+echo ====== TEST Done ======i
